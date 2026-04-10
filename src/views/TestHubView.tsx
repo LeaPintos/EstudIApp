351d7f057a65439fabb888846e3195a736c44632
Leandro Pintos <104126700+LeaPintos@users.noreply.github.com>
2026-04-09 21:03:53 -0300

    Autonomous push - Final batch (Testing & Assets)

---
+++ src/views/TestHubView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestHubView: React.FC = () => {
+  return (
+    <div>
+      <h2>TEST HUB</h2>
+    </div>
+  );
+};
+
+export default TestHubView;
---
+++ src/views/TestModeView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestModeView: React.FC = () => {
+  return (
+    <div>
+      <h2>MODO TEST</h2>
+    </div>
+  );
+};
+
+export default TestModeView;
